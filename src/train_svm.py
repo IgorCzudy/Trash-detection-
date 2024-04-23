6a6b0ec65de127e7e7e32b0eada6d9c0a6f1a9fc
Anna Panfil <anna.panfil@student.put.poznan.pl>
2024-04-23 16:44:21 +0200

    feat: compare svm and roboflow precision and inference time

--- src/train_svm.py
+++ src/train_svm.py
@@ -3,13 +3,13 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.preprocessing import StandardScaler
+import pickle
 
 
 def get_X_Y(data, verb=False, split=""):
     data = data[1:, :]
 
-    rectangles = data[:, 0:4]
-    X = data[:, 4:-1]
+    X = data[:, :-1]
     Y = data[:, -1]
     unique_values, counts = np.unique(Y, return_counts=True)
 
@@ -19,13 +19,10 @@ def get_X_Y(data, verb=False, split=""):
         for value, count in zip(unique_values, counts):
             print(f"{classes[value]}: {count}")
         
-    return X, Y, rectangles
+    return X, Y
 
 
-def print_scores(X, Y, rectangles, classifier, split=""):
-    
-
-
+def print_scores(X, Y, classifier, split=""):
     Y_pred = classifier.predict(X)
     accuracy = accuracy_score(Y, Y_pred)
     precision = precision_score(Y, Y_pred)
@@ -47,21 +44,23 @@ def train_and_test(verb=False):
     val_data = np.loadtxt('data/feature_vectors_validation.csv', delimiter=',')
     test_data = np.loadtxt('data/feature_vectors_test.csv', delimiter=',')
 
-    X_train, Y_train, rectangles_train = get_X_Y(train_data, verb, split="train")
-    X_val, Y_val, rectangles_val = get_X_Y(val_data, verb, split="validation")
-    X_test, Y_test, rectangles_test = get_X_Y(test_data, verb, split="test")
+    X_train, Y_train = get_X_Y(train_data, verb, split="train")
+    X_val, Y_val = get_X_Y(val_data, verb, split="validation")
+    X_test, Y_test = get_X_Y(test_data, verb, split="test")
 
     print("\n")
 
-    # Without scaler
-    svm_classifier = SVC(kernel='linear', random_state=42, class_weight="balanced")
-    svm_classifier.fit(X_train, Y_train)
+    svm_classifier = SVC(kernel='rbf', random_state=42, class_weight="balanced", C=1.2)
 
-    scores_string = ""
-    scores_string += print_scores(X_train, Y_train, rectangles_train, svm_classifier, split="train")
-    scores_string += print_scores(X_val, Y_val, svm_classifier, split="validation")
-    scores_string += print_scores(X_test, Y_test, svm_classifier, split="test")
-    print(scores_string)
+    # # Without scaler
+    # print("WITHOUT SCALER\n")
+    # svm_classifier.fit(X_train, Y_train)
+
+    # scores_string = ""
+    # scores_string += print_scores(X_train, Y_train, svm_classifier, split="train")
+    # scores_string += print_scores(X_val, Y_val, svm_classifier, split="validation")
+    # scores_string += print_scores(X_test, Y_test, svm_classifier, split="test")
+    # print(scores_string)
 
 
     # With scaler
@@ -80,4 +79,18 @@ def train_and_test(verb=False):
     print(scores_string)
 
 if __name__ == "__main__":
-    train_and_test(verb=True)
+    # train_and_test(verb=True)
+
+    # train and save the best model
+    train_data = np.loadtxt('data/feature_vectors_training.csv', delimiter=',')
+    X_train, Y_train = get_X_Y(train_data, verb=False, split="train")
+
+    svm_classifier = SVC(kernel='rbf', random_state=42, class_weight="balanced", C=1.2)
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    svm_classifier.fit(X_train_scaled, Y_train)
+    
+    with open('out/svc_model.pkl', 'wb') as f:
+        pickle.dump((scaler, svm_classifier), f)
+
+
